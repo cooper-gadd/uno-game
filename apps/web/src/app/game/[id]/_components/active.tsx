a3ff3e8271f45579c5479c2d57015d65fa128cbd
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2024-12-07 21:19:59 -0500

    active layout

--- apps/web/src/app/game/[id]/_components/active.tsx
+++ apps/web/src/app/game/[id]/_components/active.tsx
@@ -1,8 +1,9 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { db } from "@/server/db";
 import { getCurrentUser } from "@/server/db/queries";
 import { redirect } from "next/navigation";
-import { UnoCard } from "./uno-card";
 import { GameChat } from "./game-chat";
+import { UnoCard } from "./uno-card";
 
 export async function Active({ gameId }: { gameId: number }) {
   const currentUser = await getCurrentUser();
@@ -32,14 +33,31 @@ export async function Active({ gameId }: { gameId: number }) {
   const playerCards = player.playerHands;
 
   return (
-    <div className="flex flex-col items-center gap-8 p-8">
-      <UnoCard card={gameState.card!} />
+    <div className="flex-1 flex-col space-y-6 p-4 md:flex">
+      <div className="flex items-center justify-center">
+        <UnoCard card={gameState.card!} />
+      </div>
       <div className="flex w-full flex-wrap justify-center gap-4">
         {playerCards.map(({ card }) => (
           <UnoCard key={card.id} card={card} />
         ))}
       </div>
-      <GameChat gameId={gameId.toString()} />
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>Game Chat</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <GameChat gameId={gameId.toString()} />
+          </CardContent>
+        </Card>
+        <Card className="col-span-3">
+          <CardHeader>
+            <CardTitle>Players</CardTitle>
+          </CardHeader>
+          <CardContent></CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
